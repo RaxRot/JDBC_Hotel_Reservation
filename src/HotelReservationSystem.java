03d7bf0ab2139f46131947aadac2a689670f44fb
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-01-16 15:25:47 +0000

    Init

---
+++ src/HotelReservationSystem.java
@@ -0,0 +1,9 @@
+public class HotelReservationSystem {
+    private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
+    private static final String user = "root";
+    private static final String password = "19649072Sever";
+
+    public static void main(String[] args) {
+
+    }
+}
